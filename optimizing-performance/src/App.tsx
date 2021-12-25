66e3efb2c30472a7d643bf42ea51a73d28d38e1b
--replace-all <k435467@gmail.com>
2021-12-25 21:51:08 +0800

    performance example

---
+++ optimizing-performance/src/App.tsx
@@ -0,0 +1,107 @@
+import React, { useCallback, useState } from "react";
+import "./App.css";
+
+// ---------
+// BLUE BOX
+// ---------
+
+interface BlueBoxProps {
+  onClick: () => void;
+}
+
+// const BlueBox = React.memo(function BlueBox({ onClick }: BlueBoxProps) {
+// const BlueBox = ({ onClick }: BlueBoxProps) => {
+const BlueBox = React.memo(function BlueBox({ onClick }: BlueBoxProps) {
+  const [num, setNum] = useState<number>(0);
+  return (
+    <div className="box blue">
+      <button
+        onClick={() => {
+          setNum((v) => v + 1);
+        }}
+      >
+        {num}
+      </button>
+      <button onClick={onClick}>onClick</button>
+    </div>
+  );
+});
+
+// ----------
+// GREEN BOX
+// ----------
+
+interface GreenBoxProps {
+  name: { firstName: string; lastName: string };
+}
+
+// const GreenBox = React.memo(function GreenBox({ name }: GreenBoxProps) {
+// const GreenBox = ({ name }: GreenBoxProps) => {
+const GreenBox = React.memo(function GreenBox({ name }: GreenBoxProps) {
+  const [num, setNum] = useState<number>(0);
+
+  // const onClick = () => {
+  //   setNum((v) => v + 1);
+  // };
+  // const onClick = useCallback(() => {
+  //   setNum(num + 1);
+  // }, []);
+  const onClick = useCallback(() => {
+    setNum((v) => v + 1);
+  }, []);
+
+  return (
+    <div className="box green">
+      <button
+        onClick={() => {
+          setNum((v) => v + 1);
+        }}
+      >
+        {num} {JSON.stringify(name)}
+      </button>
+      <BlueBox onClick={onClick} />
+    </div>
+  );
+});
+
+// ---------
+// RED BOX
+// ---------
+
+interface RedBoxProps {
+  ok?: boolean;
+}
+
+const RedBox = ({ ok }: RedBoxProps) => {
+  const [num, setNum] = useState<number>(0);
+
+  // const name = { firstName: "foo", lastName: "bar" };
+  const name = React.useMemo(() => ({ firstName: "foo", lastName: "bar" }), []);
+
+  return (
+    <div className="box red">
+      <button
+        onClick={() => {
+          setNum((v) => v + 1);
+        }}
+      >
+        {num} {ok}
+      </button>
+      <GreenBox name={name} />
+    </div>
+  );
+};
+
+// ----
+// APP
+// ----
+
+function App() {
+  return (
+    <div className="App">
+      <RedBox />
+    </div>
+  );
+}
+
+export default App;
